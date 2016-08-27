a4705144202bc8f08f91c1859483d152b986bfb6
no such user <no@such.user>
2016-08-28 01:29:39 +0200

    I'm not sure about puzzle 2 input conditions...

--- BookPuzzles/BookPuzzles/Chapter1.fs
+++ BookPuzzles/BookPuzzles/Chapter1.fs
@@ -5,3 +5,7 @@ let puzzle1() =
     let solution = positives |> Seq.tryFind (fun x -> x % 3 = 0 && x % 4 = 0 && x % 5 = 0)
     printfn "Puzzle 1: Number of days: %i" (Option.get solution)
 
+let puzzle2() = 
+//TODO - can we render name of the puzzle automatically?
+    printfn "Puzzle 2: 0? Because eventually they'll all are good?"
+
--- BookPuzzles/BookPuzzles/Program.fs
+++ BookPuzzles/BookPuzzles/Program.fs
@@ -6,6 +6,7 @@ let main argv =
     printfn "Chapter 1:"
     //TODO - can we introduce automatic indent for each chapter?
     Chapter1.puzzle1()
+    Chapter1.puzzle2()
 
     System.Console.WriteLine "Done. Press enter.."
     System.Console.ReadLine() |> ignore
